54520774e27ce2868649e1485c0597640d7f8d3c
eds2002 <es23jr@gmail.com>
2023-07-21 17:50:13 -0400

    maintenance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 0.8,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-  });
-
-  return <Component {...pageProps} />;
+  return (
+    <section className="flex items-center justify-center h-screen bg-white">
+      <div className="max-w-screen-md px-4 py-8 mx-auto text-center lg:py-16 lg:px-12">
+        <h1 className="mb-4 text-2xl font-bold leading-none tracking-tight lg:mb-6 md:text-2xl xl:text-4xl ">
+          Sanchez Family Cleaning
+        </h1>
+        <h2 className="mb-4 text-4xl font-bold leading-none tracking-tight lg:mb-6 md:text-5xl xl:text-6xl ">
+          Under Maintenance
+        </h2>
+        <p className="font-light text-gray-500 md:text-lg xl:text-xl ">
+          We&apos;re under construction, we can&apos;t wait to show you what
+          we&apos;re working on.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default MyApp;
